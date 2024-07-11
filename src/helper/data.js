b3328c2321f8dfbfa2a8c4e6af28f689ebd7b23c
ashika <nepalashika1@gmail.com>
2024-07-11 13:41:26 +0545

    small page

---
+++ src/helper/data.js
@@ -0,0 +1,59 @@
+export const landingFooterLinks=[{
+    id:"1",
+    title:"Facebook",
+    link:"https://facebook.com",
+},
+{
+id:"2",
+title:"Twitter",
+link:"https://twitter.com",
+},
+{
+id:"3",
+title:"Youtube",
+link:"https://Youtube.com",
+},
+{
+id:"4",
+title:"X",
+link:"https://x.com",
+},
+{
+    id:"5",
+    title:"Instagram",
+    link:"https://Instagram.com",
+    },
+];
+
+export const landingHeaderLinks = [
+    {
+      id: "1",
+      title: "About",
+      link: "#",
+    },
+    {
+      id: "2",
+      title: "Tours",
+      link: "#",
+    },
+    {
+      id: "3",
+      title: "sale",
+      link: "#",
+    },
+    {
+      id: "4",
+      title: "Contact",
+      link: "#",
+    },
+    {
+      id: "5",
+      title: "Sign In",
+      link: "/signin",
+    },
+    {
+      id: "6",
+      title: "Sign Up",
+      link: "/signup",
+    },
+  ];
